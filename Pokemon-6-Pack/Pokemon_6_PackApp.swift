e183143abbae5554c8da0b666fbd26f4931e8e0a
Кривдин Артем <akrivdin@data-platform.ru>
2023-02-04 15:25:09 +0300

    Initial Commit

---
+++ Pokemon-6-Pack/Pokemon_6_PackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_6_PackApp.swift
+//  Pokemon-6-Pack
+//
+//  Created by Артем Кривдин on 04.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_6_PackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
